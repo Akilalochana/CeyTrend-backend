b2df89c98f83b02c632b2aa843e740cd9f1b2031
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-06-05 22:35:41 +0530

    login user and implement JWT token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export function registerUser(req, res){
   const data = req.body;
 
   data.password = bcrypt.hashSync(data.password, 10);
-  
+
   const newUser = new User(data);
 
   newUser.save().then(()=>{
@@ -16,5 +17,40 @@ export function registerUser(req, res){
     res.status(500).json({
       message:"User registration failed"
     })
+  })
+}
+
+export function loginUser(req, res){
+  const data = req.body;
+  User.findOne({
+    email:data.email
+  }).then((user)=>{
+    if(user==null){
+      res.status(404).json({
+        message:"User not found"
+      })
+
+    }else{
+     const correctPassword = bcrypt.compareSync(data.password, user.password);
+
+     if(correctPassword){
+
+      const token = jwt.sign({
+        name:user.firstName,
+        lastName:user.lastName,
+        email:user.email,
+        role:user.role
+      }, "cws2003")
+      
+      res.status(200).json({
+        message:"User logged in successfully",
+        token:token
+      })
+     }else{
+      res.status(401).json({
+        message:"login failed!"
+      })
+     }
+    }
   })
 }
