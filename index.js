eaf62a67a5066b7246c0daa2309ab21a39d0de1a
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-06-06 06:29:41 +0530

    add dotenv and gitignore

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();// load env file
 
 let app = express();
 
@@ -23,7 +26,7 @@ app.use((req, res, next)=>{
   next();
 });
 
-let mongoUrl = "mongodb+srv://admin:123@cluster0.mwn2w7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+let mongoUrl = process.env.MONGO_URL
 
 mongoose.connect(mongoUrl)
 
